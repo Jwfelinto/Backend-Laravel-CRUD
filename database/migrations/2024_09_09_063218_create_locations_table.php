febe9e887b27cd376336aee1ad9bf0885c701716
Jwfelinto <jwfelinto@gmail.com>
2024-09-09 03:38:42 -0300

    feat: Criando migration para tabela de locations.

---
+++ database/migrations/2024_09_09_063218_create_locations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLocationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        if (!Schema::hasTable('locations')) {
+            Schema::create('locations', function (Blueprint $table) {
+                $table->id();
+                $table->string('uf', 2)->unique();
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('locations');
+    }
+}
