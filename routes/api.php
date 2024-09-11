03ecdf7609830227127e9120e98ba23323af375c
Jwfelinto <jwfelinto@gmail.com>
2024-09-11 09:36:01 -0300

    feat: Criando as rotas.

---
+++ routes/api.php
@@ -0,0 +1,52 @@
+<?php
+
+use App\Http\Controllers\ClientController;
+use App\Http\Controllers\InstallationTypesController;
+use App\Http\Controllers\LocationController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\ToolController;
+use Illuminate\Support\Facades\Route;
+
+/**
+ *
+ */
+Route::prefix('/clientes')->name('client.')->group(function () {
+    Route::get('/', [ClientController::class, 'index'])->name('index');
+    Route::get('/{clients}', [ClientController::class, 'show'])->name('show');
+    Route::post('/', [ClientController::class, 'store'])->name('store');
+    Route::put('/{clients}', [ClientController::class, 'update'])->name('update');
+    Route::patch('/{clients}', [ClientController::class, 'destroy'])->name('destroy');
+});
+
+/**
+ * Installation Types Routes
+ */
+Route::prefix('/instalação')->name('installation.')->group(function () {
+    Route::get('/', [InstallationTypesController::class, 'index'])->name('index');
+});
+
+/**
+ * Location Routes
+ */
+Route::prefix('/locais')->name('location.')->group(function () {
+    Route::get('/', [LocationController::class, 'index'])->name('index');
+});
+
+/**
+ * Project Routes
+ */
+Route::prefix('/projetos')->name('project.')->group(function () {
+    Route::get('/{project}/equipamentos', [ProjectController::class, 'projectTools'])->name('projectTools');
+    Route::get('/', [ProjectController::class, 'index'])->name('index');
+    Route::get('/{project}', [ProjectController::class, 'show'])->name('show');
+    Route::post('/', [ProjectController::class, 'store'])->name('store');
+    Route::put('/{project}', [ProjectController::class, 'update'])->name('update');
+    Route::patch('/{project}', [ProjectController::class, 'destroy'])->name('destroy');
+});
+
+/**
+ * Tool Routes
+ */
+Route::prefix('/equipamentos')->name('tools.')->group(function () {
+    Route::get('/', [ToolController::class, 'index'])->name('index');
+});
