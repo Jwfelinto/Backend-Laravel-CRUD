75429ef458d80011622456a1cd7f453c2fcb0ec2
Jwfelinto <jwfelinto@gmail.com>
2024-09-11 08:40:00 -0300

    feat: Criando 'InstallationTypesController' com o método 'index'.

--- app/Http/Controllers/InstallationTypesController.php
+++ app/Http/Controllers/InstallationTypesController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Resources\InstallationTypeResource;
+use App\Services\InstallationTypeService;
+use Illuminate\Http\Resources\Json\JsonResource;
 
 class InstallationTypesController extends Controller
 {
-    //
+    private InstallationTypeService $installationService;
+
+    /**
+     * @param InstallationTypeService $installationService
+     */
+    public function __construct(InstallationTypeService $installationService)
+    {
+        $this->installationService = $installationService;
+    }
+
+    /**
+     * @return JsonResource
+     */
+    public function index(): JsonResource
+    {
+        $installationTypes = $this->installationService->getAll();
+
+        return InstallationTypeResource::collection($installationTypes);
+    }
 }
