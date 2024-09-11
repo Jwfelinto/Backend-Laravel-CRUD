98ab219e2280fe2c4cd1fd21e0fbc895f4f813f8
Jwfelinto <jwfelinto@gmail.com>
2024-09-11 07:45:55 -0300

    feat: Criando o 'ClientRequest' e suas validações.

---
+++ app/Http/Requests/ClientRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class ClientRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email',
+            'phone' => 'required|string|max:15',
+            'cpf_cnpj' => 'required|string|max:14',
+        ];
+    }
+}
