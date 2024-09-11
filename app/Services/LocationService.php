ced8f42012c7476897001a2c6db30cd3cb82b180
Jwfelinto <jwfelinto@gmail.com>
2024-09-11 09:21:46 -0300

    feat: Tipando retorno do método 'getAll' e adicionando notations.

--- app/Services/LocationService.php
+++ app/Services/LocationService.php
@@ -3,17 +3,24 @@
 namespace App\Services;
 
 use App\Repositories\Interfaces\LocationRepositoryInterface;
+use Illuminate\Database\Eloquent\Collection;
 
 class LocationService
 {
     private LocationRepositoryInterface $locationRepository;
 
+    /**
+     * @param LocationRepositoryInterface $locationRepository
+     */
     public function __construct(LocationRepositoryInterface $locationRepository)
     {
         $this->locationRepository = $locationRepository;
     }
 
-    public function getAll()
+    /**
+     * @return Collection
+     */
+    public function getAll(): Collection
     {
         return $this->locationRepository->all();
     }
