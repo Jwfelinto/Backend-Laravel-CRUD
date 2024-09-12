740367edfe0595b8d4e2df1013625a567363d7ee
Jwfelinto <jwfelinto@gmail.com>
2024-09-12 13:17:47 -0300

    feat - Adicionando relacionamentos na model 'Project'.

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Project extends Model
 {
@@ -14,8 +16,22 @@ class Project extends Model
         'location_id',
         'installation_type_id',
     ];
+    public function client(): BelongsTo
+    {
+        return $this->belongsTo(Client::class);
+    }
 
-    public function tools()
+    public function location(): BelongsTo
+    {
+        return $this->belongsTo(Location::class);
+    }
+
+    public function installationType(): BelongsTo
+    {
+        return $this->belongsTo(InstallationType::class, 'installation_type_id');
+    }
+
+    public function tools(): BelongsToMany
     {
         return $this->belongsToMany(Tool::class, 'project_tools')
             ->withPivot('quantity');
