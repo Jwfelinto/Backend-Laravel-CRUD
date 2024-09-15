7440d5d2e7cc1b9f7653a0e047548aadde3eede1
Jwfelinto <jwfelinto@gmail.com>
2024-09-14 21:24:52 -0300

    feat- Adicionando novos métodos de validação para os métodos 'store' e 'update' do 'ClientRequest'.

--- app/Http/Requests/ClientRequest.php
+++ app/Http/Requests/ClientRequest.php
@@ -3,7 +3,9 @@
 namespace App\Http\Requests;
 
 use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class ClientRequest extends FormRequest
 {
@@ -14,11 +16,57 @@ class ClientRequest extends FormRequest
      */
     public function rules(): array
     {
+        if ($this->isMethod('post')) {
+            return $this->rulesForStore();
+        }
+
+        if ($this->isMethod('put') || $this->isMethod('patch')) {
+            return $this->rulesForUpdate();
+        }
+
+        return [];
+    }
+
+    /**
+     * @return string[]
+     */
+    public function rulesForStore(): array
+    {
         return [
             'name' => 'required|string|max:255',
-            'email' => 'required|email|max:100|unique:clients,email,' . $this->client->id,
+            'email' => 'required|email|max:100|unique:clients,email',
             'phone' => 'required|string|max:20',
-            'cpf_cnpj' => 'required|string|unique:clients,cpf_cnpj,' . $this->client->id,
+            'cpf_cnpj' => 'required|string|unique:clients,cpf_cnpj',
         ];
+    }
+
+    /**
+     * @return string[]
+     */
+    public function rulesForUpdate(): array
+    {
+        $clientId = $this->route('client')->id;
+
+        return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:100|unique:clients,email,' . $clientId,
+            'phone' => 'required|string|max:20',
+            'cpf_cnpj' => 'required|string|unique:clients,cpf_cnpj,' . $clientId,
+        ];
+    }
+
+
+    /**
+     * @param Validator $validator
+     * @return mixed
+     */
+    protected function failedValidation(Validator $validator): mixed
+    {
+        throw new HttpResponseException(
+            response()->json([
+                'message' => 'Erro de validação',
+                'errors' => $validator->errors(),
+            ], 422)
+        );
     }
 }
