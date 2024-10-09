cd1afc01892bb05f0f069101ea25083efe34a3f1
Jwfelinto <jwfelinto@gmail.com>
2024-10-09 19:47:32 -0300

    feat-Criando a classe 'LoginRequest'.

---
+++ app/Http/Requests/LoginRequest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class LoginRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'messages' => $validator->getMessageBag(),
+        ], 400));
+    }
+}
