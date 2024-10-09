10371d6db6188d4a1aad1368861a714818d0f2fd
Jwfelinto <jwfelinto@gmail.com>
2024-10-09 18:03:48 -0300

    feat-Criando a classe 'UserService' com os métodos  'getAll', 'registerUser', 'updateUser'.

---
+++ app/Services/UserService.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Services;
+
+use App\Models\User;
+use App\Repositories\Interfaces\UserRepositoryInterface;
+use Illuminate\Database\Eloquent\Collection;
+
+class UserService
+{
+    /**
+     * @var UserRepositoryInterface
+     */
+    private UserRepositoryInterface $userRepository;
+
+    /**
+     * @param UserRepositoryInterface $userRepository
+     */
+    public function __construct(UserRepositoryInterface $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    /**
+     * @param array|null $data
+     * @return Collection
+     */
+    public function getAll(?array $data): Collection
+    {
+        return $this->userRepository->all($data);
+    }
+
+    /**
+     * @param array $data
+     * @return User
+     */
+    public function registerUser(array $data): User
+    {
+        $user = new User([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+
+        return $this->userRepository->create($user);
+    }
+
+    /**
+     * @param array $data
+     * @param User $user
+     * @return User
+     */
+    public function updateUser(array $data, User $user): User
+    {
+        $user->name = $data['name'];
+        $user->email = $data['email'];
+        $user->password = bcrypt($data['password']);
+
+        return $this->userRepository->update($user);
+    }
+
+}
