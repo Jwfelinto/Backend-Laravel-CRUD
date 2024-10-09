b5dd8cdfa06fb3949ad8d8532d6662ba8bd26132
Jwfelinto <jwfelinto@gmail.com>
2024-10-09 18:34:06 -0300

    feat-Criando o 'UserRequest' com os métodos 'rules', 'rulesForUpdate' e 'failedValidation'.

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
+
+/**
+ * @OA\Schema(
+ *     schema="ClientRequest",
+ *     required={"name", "email", "password"},
+ *     @OA\Property(property="name", type="string", example="John Doe"),
+ *     @OA\Property(property="email", type="string", example="john@example.com"),
+ *     @OA\Property(property="password", type="string", example="password"),
+ * )
+ */
+class UserRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        if ($this->isMethod('put')) {
+            return $this->rulesForUpdate();
+        }
+
+        return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|max:100|unique:users',
+            'password' => 'required|string|min:10'
+        ];
+    }
+
+    /**
+     * @return string[]
+     */
+    public function rulesForUpdate(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email', Rule::unique('users')->ignore($this->id),
+            'password' => 'required|string|min:10'
+        ];
+    }
+
+
+    /**
+     * @param Validator $validator
+     * @return mixed
+     */
+    protected function failedValidation(Validator $validator): mixed
+    {
+        throw new HttpResponseException(
+            response()->json([
+                'message' => 'Erro de validação',
+                'errors' => $validator->errors(),
+            ], 422)
+        );
+    }
+}
