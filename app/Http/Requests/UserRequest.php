3ffbe8114a3a233c8d06a51a9086eea1d37c7426
Jwfelinto <jwfelinto@gmail.com>
2026-07-19 02:12:30 -0300

    feat-Adjust on UserRequest.

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -3,11 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Contracts\Validation\ValidationRule;
-use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Http\Exceptions\HttpResponseException;
-use Illuminate\Http\JsonResponse;
-use Illuminate\Validation\Rule;
 
 class UserRequest extends FormRequest
 {
@@ -18,75 +14,10 @@ class UserRequest extends FormRequest
      */
     public function rules(): array
     {
-        if ($this->isMethod('put')) {
-            return $this->rulesForUpdate();
-        }
-
         return [
             'name' => 'required|string|max:255',
             'email' => 'required|email|max:100|unique:users',
-            'password' => [
-                'required',
-                'string',
-                'min:10'
-            ],
+            'password' => 'required|string|min:10',
         ];
-    }
-
-    /**
-     * @return string[]
-     */
-    public function rulesForUpdate(): array
-    {
-        return [
-            'name' => 'required|string|max:255',
-            'email' => 'required|email', Rule::unique('users')->ignore($this->id),
-            'password' => [
-                'required',
-                'string',
-                'min:10'
-            ],
-        ];
-    }
-
-    /**
-     * @param Validator $validator
-     * @return mixed
-     */
-    protected function failedValidation(Validator $validator): JsonResponse
-    {
-        throw new HttpResponseException(
-            response()->json([
-                'message' => 'Erro de validação',
-                'errors' => $validator->errors(),
-            ], 422)
-        );
-    }
-
-    /**
-     * @param Validator $validator
-     * @return void
-     */
-    protected function withValidator(Validator  $validator): void
-    {
-        $validator->after(function ($validator) {
-            $password = $this->input('password');
-
-            if (!preg_match('/[a-z]/', $password)) {
-                $validator->errors()->add('password', 'A senha deve conter pelo menos uma letra minúscula (a-z).');
-            }
-
-            if (!preg_match('/[A-Z]/', $password)) {
-                $validator->errors()->add('password', 'A senha deve conter pelo menos uma letra maiúscula (A-Z).');
-            }
-
-            if (!preg_match('/[0-9]/', $password)) {
-                $validator->errors()->add('password', 'A senha deve conter pelo menos um número (0-9).');
-            }
-
-            if (!preg_match('/[!@#$%^&*]/', $password)) {
-                $validator->errors()->add('password', 'A senha deve conter pelo menos um caractere especial (!@#$%^&*).');
-            }
-        });
     }
 }
