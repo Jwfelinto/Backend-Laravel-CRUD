0ab4e6c1c58e5c19608086362958fe3db3a11544
Jwfelinto <jwfelinto@gmail.com>
2026-07-19 01:25:27 -0300

    feat- Create ProjectsResource class.

---
+++ app/Http/Resources/ProjectsResource.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ProjectsResource extends JsonResource
+{
+    /**
+     * @param Request $request
+     * @return array
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'installation_type' => $this->installationType->name
+        ];
+    }
+}
