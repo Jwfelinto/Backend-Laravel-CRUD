e36e7407616867ce38e5688b26d7631aec1eed6c
Jwfelinto <jwfelinto@gmail.com>
2026-07-19 00:19:37 -0300

    feat- Adjust on StoreClientRequest class.

--- app/Http/Requests/StoreClientRequest.php
+++ app/Http/Requests/StoreClientRequest.php
@@ -3,12 +3,8 @@
 namespace App\Http\Requests;
 
 use Illuminate\Contracts\Validation\ValidationRule;
-use Illuminate\Contracts\Validation\Validator;
-use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Http\Exceptions\HttpResponseException;
-use Illuminate\Validation\Rule;
 
-class StoreClientRequest extends FormRequest
+class StoreClientRequest extends BaseFormRequest
 {
     /**
      * Get the validation rules that apply to the request.
@@ -17,43 +13,11 @@ class StoreClientRequest extends FormRequest
      */
     public function rules(): array
     {
-        if ($this->isMethod('put')) {
-            return $this->rulesForUpdate();
-        }
-
         return [
             'name' => 'required|string|max:255',
             'email' => 'required|email|max:100|unique:clients,email',
             'phone' => 'required|string|max:20',
             'cpf_cnpj' => 'required|string|unique:clients,cpf_cnpj',
         ];
-    }
-
-    /**
-     * @return string[]
-     */
-    public function rulesForUpdate(): array
-    {
-        return [
-            'name' => 'required|string|max:255',
-            'email' => 'required|email', Rule::unique('clients')->ignore($this->id),
-            'phone' => 'required|string|max:20',
-            'cpf_cnpj' => 'required|string', Rule::unique('clients')->ignore($this->id),
-        ];
-    }
-
-
-    /**
-     * @param Validator $validator
-     * @return mixed
-     */
-    protected function failedValidation(Validator $validator): mixed
-    {
-        throw new HttpResponseException(
-            response()->json([
-                'message' => 'Erro de validação',
-                'errors' => $validator->errors(),
-            ], 422)
-        );
     }
 }
