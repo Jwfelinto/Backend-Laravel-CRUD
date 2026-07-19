5f1f3883adafda0aa1398701c969065ee02addc2
Jwfelinto <jwfelinto@gmail.com>
2026-07-19 02:16:00 -0300

    feat-Adjust on UpdateUserRequest.

--- app/Http/Requests/UpdateClientRequest.php
+++ app/Http/Requests/UpdateClientRequest.php
@@ -3,10 +3,9 @@
 namespace App\Http\Requests;
 
 use Illuminate\Contracts\Validation\ValidationRule;
-use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
 
-class UpdateClientRequest extends FormRequest
+class UpdateClientRequest extends BaseFormRequest
 {
     /**
      * Get the validation rules that apply to the request.
