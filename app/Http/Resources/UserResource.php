9dca636771bc08c50c590ca29b50cd56b9a2c997
Jwfelinto <jwfelinto@gmail.com>
2026-07-19 02:18:18 -0300

    feat-Adjust on UserResource.

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -4,7 +4,6 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
-use OpenApi\Annotations as OA;
 
 class UserResource extends JsonResource
 {
@@ -16,26 +15,10 @@ class UserResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        if ($request->routeIs('user.index')) {
-            return $this->toArrayCollection($request);
-        }
-
         return [
             'id' => $this->id,
             'name' => $this->name,
             'email' => $this->email
-        ];
-    }
-
-    /**
-     * @param Request $request
-     * @return array
-     */
-    private function toArrayCollection(Request $request): array
-    {
-        return [
-            'id' => $this->id,
-            'name' => $this->name
         ];
     }
 }
