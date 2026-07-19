4d13efc6b57504af67188f766ad3af716264a0f7
Jwfelinto <jwfelinto@gmail.com>
2026-07-18 23:56:16 -0300

    feat- Adjust on ClientResource class.

--- app/Http/Resources/ClientResource.php
+++ app/Http/Resources/ClientResource.php
@@ -15,10 +15,6 @@ class ClientResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        if ($request->routeIs('client.index')) {
-            return $this->toArrayCollection($request);
-        }
-
         return [
             'id' => $this->id,
             'name' => $this->name,
@@ -26,18 +22,6 @@ class ClientResource extends JsonResource
             'phone' => $this->phone,
             'cpf_cnpj' => $this->cpf_cnpj,
             'projects' => ProjectResource::collection($this->projects)
-        ];
-    }
-
-    /**
-     * @param Request $request
-     * @return array
-     */
-    private function toArrayCollection(Request $request): array
-    {
-        return [
-            'id' => $this->id,
-            'name' => $this->name
         ];
     }
 }
