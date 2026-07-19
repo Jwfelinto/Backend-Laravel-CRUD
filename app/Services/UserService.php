af6131c0f2bf97d3902f36b4d18fa633638ab2db
Jwfelinto <jwfelinto@gmail.com>
2026-07-19 02:58:51 -0300

    feat-Create method fillUser in UserService.

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -9,64 +9,45 @@ use Illuminate\Support\Facades\Hash;
 
 class UserService
 {
-    /**
-     * @var UserRepositoryInterface
-     */
     private UserRepositoryInterface $userRepository;
 
-    /**
-     * @param UserRepositoryInterface $userRepository
-     */
     public function __construct(UserRepositoryInterface $userRepository)
     {
         $this->userRepository = $userRepository;
     }
 
-    /**
-     * @param array|null $data
-     * @return LengthAwarePaginator
-     */
     public function getAll(?array $data): LengthAwarePaginator
     {
         return $this->userRepository->all($data);
     }
 
-    /**
-     * @param array $data
-     * @return User
-     */
     public function registerUser(array $data): User
     {
-        $user = new User([
-            'name' => $data['name'],
-            'email' => $data['email'],
-            'password' => Hash::make($data['password']),
-        ]);
-
-        return $this->userRepository->create($user);
+        return $this->userRepository->create(
+            $this->fillUser(new User(), $data)
+        );
     }
 
-    /**
-     * @param array $data
-     * @param User $user
-     * @return User
-     */
     public function updateUser(array $data, User $user): User
     {
-        $user->name = $data['name'];
-        $user->email = $data['email'];
-        $user->password = Hash::make($data['password']);
-
-        return $this->userRepository->update($user);
+        return $this->userRepository->update(
+            $this->fillUser($user, $data)
+        );
     }
 
-    /**
-     * @param User $user
-     * @return void
-     */
     public function deleteUser(User $user): void
     {
         $user->delete();
     }
 
+    private function fillUser(User $user, array $data): User
+    {
+        $user->fill([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+
+        return $user;
+    }
 }
